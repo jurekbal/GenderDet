a47be2937146fa3378ecbbe2c8a36ed305ab20ad
Jerzy Balwinski <jurek_b@wp.pl>
2021-04-05 19:37:16 +0200

    Rest controller implementation and simple test case

--- src/main/java/com/balwinski/genderdet/api/GenderDetController.java
+++ src/main/java/com/balwinski/genderdet/api/GenderDetController.java
@@ -1,15 +1,35 @@
 package com.balwinski.genderdet.api;
 
+import com.balwinski.genderdet.services.FirstTokenGenderDetector;
+import com.balwinski.genderdet.services.GenderDetector;
+import com.balwinski.genderdet.services.MultiTokenGenderDetector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/genderdet")
 public class GenderDetController {
 
+    private GenderDetector detector;
+
     @GetMapping
-    public String getGender() {
-        return "Gender controller works";
+    public String getGender(@RequestParam(value="name", defaultValue = "") String name,
+                            @RequestParam(value = "method", defaultValue = "1") int method) {
+        switch (method) {
+            case 1: {
+                detector = new FirstTokenGenderDetector();
+                break;
+            }
+            case 2: {
+                detector = new MultiTokenGenderDetector();
+                break;
+            }
+            default: {
+                detector = new FirstTokenGenderDetector();
+            }
+        }
+        return detector.detect(name);
     }
 }
